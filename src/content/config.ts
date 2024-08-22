e49e7b3c8ea23edcb554d8d6e4bdb3823f6fd566
Laweeza <louieshing@gmail.com>
2024-08-22 00:34:34 -0700

    updated errors

--- src/content/config.ts
+++ src/content/config.ts
@@ -27,6 +27,7 @@ const projects = defineCollection({
     description: z.string(),
     date: z.coerce.date(),
     draft: z.boolean().optional(),
+    demoURL: z.string().optional(),
     repoURL: z.string().optional(),
   }),
 });
